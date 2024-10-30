843ddb8da0709dd36b421cc861f1ce708bf4ebdb
Notuwapu <prawirasaragih1@gmail.com>
2024-10-30 22:58:35 +0700

    Push menambah back button di preview

--- app/src/main/java/com/example/utslab/PreviewActivity.kt
+++ app/src/main/java/com/example/utslab/PreviewActivity.kt
@@ -1,6 +1,7 @@
 package com.example.utslab
 
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,21 +12,28 @@ class PreviewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
 
+        // Inisialisasi komponen UI
         val previewName = findViewById<TextView>(R.id.previewName)
         val previewDate = findViewById<TextView>(R.id.previewDate)
         val previewMessage = findViewById<TextView>(R.id.previewMessage)
         val previewBorder = findViewById<ImageView>(R.id.previewBorder)
+        val backButton = findViewById<ImageButton>(R.id.backButton)
 
-
+        // Mendapatkan data dari intent
         val name = intent.getStringExtra("name")
         val date = intent.getStringExtra("date")
         val message = intent.getStringExtra("message")
         val border = intent.getIntExtra("border", R.drawable.border1)
 
-
+        // Menampilkan data di UI
         previewName.text = name
         previewDate.text = date
         previewMessage.text = message
         previewBorder.setImageResource(border)
+
+        // untuk tombol kembali
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
